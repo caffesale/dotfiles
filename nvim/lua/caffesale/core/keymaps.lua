48379ce56a433f74c7431d65e4c908ab9784430e
pglibrary80 <pglibrary80@gmail.com>
2023-05-21 22:44:22 +0900

    docs: nvim init

---
+++ nvim/lua/caffesale/core/keymaps.lua
@@ -0,0 +1,53 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- general keymap
+
+keymap.set("n", "x", '"_x')
+
+-- line insert
+
+keymap.set("n", "<leader>o", "o<ESC>")
+keymap.set("n", "<leader>O", "O<ESC>")
+
+-- line move
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+keymap.set({"n", "v"}, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
+keymap.set({"n", "v"}, "<leader>bd", [["_d]])
+
+keymap.set("n", "Q", "<nop>")
+keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- plugin keymaps
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
